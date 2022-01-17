6292c78ba1c9fe0abb8f9781561173045a70ded9
Mohammad Muhib <mohammadmuhib@Mohammads-MacBook-Air.local>
2022-01-17 18:56:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+//Route Handler
+app.get("/", (req,res)=>{
+  res.send ({hi: 'there'});
+});
+
+//app = Express App to register this route handler with
+//get = watch for incoming requests with this method
+// '/' = watch for request trying to access '/'
+//req = object representing the incoming requests
+//res = object representing the outgoing response
+
+//get = Get info
+//post = send info
+// put = update all the properties of something
+//delete = Deleting something
+//patch = Update one or two properties of something
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function(){
+  console.log("Server started at port 5000");
+});
